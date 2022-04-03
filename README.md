87e90fad0d23194e3db7a622f51cbdff479303a7
Truman Kilen <t@kilen.me>
2022-04-03 15:44:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Custom Difficulties
+
+This is collection of difficulties to be used with the [Custom Difficulty](https://drg.mod.io/custom-difficulty) mod for Deep Rock Galactic.
+
+## [Hazard 1](difficulties/Hazard_1.cd.json)
+Vanilla Hazard 1
+
+## [Hazard 2](difficulties/Hazard_2.cd.json)
+Vanilla Hazard 2
+
+## [Hazard 3](difficulties/Hazard_3.cd.json)
+Vanilla Hazard 3
+
+## [Hazard 4](difficulties/Hazard_4.cd.json)
+Vanilla Hazard 4
+
+## [Hazard 5](difficulties/Hazard_5.cd.json)
+Vanilla Hazard 5
+
+## [Hazard 6](difficulties/Hazard_6.cd.json) by Ike
+[Mod.io page](https://drg.mod.io/ike)
+
+## [Hazard 7](difficulties/Hazard_7.cd.json) by Ike
+[Mod.io page](https://drg.mod.io/hazard-7)
+
+## [Hazard 5 - Praetorian Edition](difficulties/Hazard_5_Praetorian_Edition.cd.json)
+This is a demonstration custom difficulty that shows how to remove grunts and create a new praetorian descriptor that takes the place of grunts. The low rarity works like the grunt rarity in that it nearly guarantees praetorians get picked first for any spawns. A future version might demonstrate adjusting spawn costs and size to increase praetorian spawn counts.
+
+## [Hazard 5 - Praetorian Edition V2](difficulties/Hazard_5_Praetorian_Edition_v2.cd.json)
+Like the other version which replaces grunts with praetorians, but here we adjust the spawn cost to match grunts, increase the ideal spawn size from 3 to 6 and remove the maximum spawn size (was 4). I've also changed the rarity to 1, which is the lowest rarity present in the EnemyDescriptors and the rarity of grunts.
+
+## [Hazard 6x2 40 Nitra](difficulties/Hazard_6x2_40_Nitra.cd.json)
+Hazard 6 with 2x enemy count, 40 nitra resupplies, and 180/180 spawn caps. Prospector and Data Vault disabled.
+
+## [Hazard 6x2 - Diversity Edition](difficulties/Hazard_6x2_Diversity_Edition.cd.json)
+This plays very similarly to normal Hazard 6x2. It's identical except that EnemeyDiversity is always the max. This results in very different enemy compositions. This variant can bring a little spice when Hazard 6x2 is feeling a little too same-y.
+
+## [Hazard 6x2 Plus](difficulties/Hazard_6x2_Plus.cd.json)
+Experimenting with adding some more enemy variants to otherwise normal hazard6x2. Currently only adds sentinels (for their aggressive digging which can disrupt otherwise comfortable positioning) and a fast but weak slasher. Subject to change.
+
+## [Hazard 6x3 40 Nitra](difficulties/Hazard_6x3_40_Nitra.cd.json)
+Hazard 6 with 3x enemy count, 40 nitra resupplies, and 180/180 spawn caps. Prospector and Data Vault disabled.
+
+## [Starship Troopers Pro](difficulties/Starship_Troopers_Pro.cd.json) by The Brain
+FOR VETERANS OF THE STARSHIP TROOPERS ELITE MOD, this mod changes spawn size, so expect to be swimming in bugs if you don't kill fast enough. The enemies still have Haz4 specifications (speed*, damage, resistance, ..) as regular Starship Troopers. I don't recommend this mod for player level below 999, and even then I saw high level players rush in the middle of a new cave, die and make the mission fail as everybody risks their live or die in rescue attempts. This is easily comparable to Hazard 9 or Hazard 10.
+
+## [Starship Troopers INSANE](difficulties/Starship_Troopers_INSANE.cd.json) by Masala
+Huge amounts of very fast enemies with very low hp, resups cost 40. Had no idea what I was doing and it's mostly a meme https://clips.twitch.tv/CogentGoodVultureVoteNay-2QOzDrdDH3kMOWyh
+
+## [Low Enemy Count](difficulties/Low_Enemy_Count.cd.json) by The Brain
+Not many bugs spawn. Difficult, but beatable.
---
+++ tools/format.js
@@ -0,0 +1,141 @@
+/**
+ * Usage: node tools/format.js <difficulty...>
+ *
+ * Sorts fields, removes default values, and formats the specified difficulties in place.
+ */
+
+const fs = require('fs');
+
+const base = JSON.parse(fs.readFileSync('base.cd.json'));
+
+const paths = process.argv.slice(2);
+
+const keys = [
+  'Name',
+  'MaxActiveCritters',
+  'MaxActiveSwarmers',
+  'MaxActiveEnemies',
+  'ResupplyCost',
+  'StartingNitra',
+  'ExtraLargeEnemyDamageResistance',
+  'ExtraLargeEnemyDamageResistanceB',
+  'ExtraLargeEnemyDamageResistanceC',
+  'ExtraLargeEnemyDamageResistanceD',
+  'EnemyDamageResistance',
+  'SmallEnemyDamageResistance',
+  'EnemyDamageModifier',
+  'EnemyCountModifier',
+  'EncounterDifficulty',
+  'StationaryDifficulty',
+  'EnemyWaveInterval',
+  'EnemyNormalWaveInterval',
+  'EnemyNormalWaveDifficulty',
+  'EnemyDiversity',
+  'StationaryEnemyDiversity',
+  'VeteranNormal',
+  'VeteranLarge',
+  'DisruptiveEnemyPoolCount',
+  'MinPoolSize',
+  'MaxActiveElites',
+  'EnvironmentalDamageModifier',
+  'PointExtractionScalar',
+  'FriendlyFireModifier',
+  'WaveStartDelayScale',
+  'SpeedModifier',
+  'AttackCooldownModifier',
+  'ProjectileSpeedModifier',
+  'HealthRegenerationMax',
+  'ReviveHealthRatio',
+  'EliteCooldown',
+  'EnemyDescriptors',
+  'EnemyPool',
+  'CommonEnemies',
+  'SpecialEnemies',
+  'DisruptiveEnemies',
+  'StationaryEnemies',
+  'SeasonalEvents',
+  'EscortMule'
+];
+
+keyOrder = new Map(keys.map((i, k) => [i, k]));
+
+function getKeys(obj) {
+  const toReturn = [];
+  const keys = Array.isArray(obj) ? obj.keys() : Object.keys(obj);
+  for (let i of keys) {
+    toReturn.push([i])
+    if ((typeof obj[i]) == 'object') {
+      for (let j of getKeys(obj[i])) {
+        toReturn.push([i, ...j]);
+      }
+    }
+  }
+  return toReturn;
+};
+
+function getKeyIndex(key, index) {
+  if (key.length <= index) throw new Error(`Index out of of bounds for key`);
+  return key[index];
+}
+
+function getKeyIndexDifficulty(key, index) {
+  if (key.length <= index) throw new Error(`Index out of of bounds for key`);
+  const v = key[index];
+  if (index === 0) {
+    if (!keyOrder.has(v)) throw new Error(`Unknown property: "${v}"`);
+    return keyOrder.get(v);
+  } else {
+    return v;
+  }
+}
+
+function compareValue(a, b) {
+  if (typeof a !== typeof b) {
+    throw new Error(`Mismatched types: "${typeof a}" and "${typeof b}"`);
+  }
+  if (typeof a === 'string') {
+    return a.localeCompare(b);
+  } else if (typeof a === 'number') {
+    return a - b;
+  }
+  throw new Error('Unknown type');
+}
+
+function getComparator(mappingFn) {
+  return (a, b) => {
+    for (let i = 0; ; i++) {
+      if (a.length !== b.length) {
+        if (a.length <= i) return -1;
+        if (b.length <= i) return 1;
+      }
+      const diff = compareValue(mappingFn(a, i), mappingFn(b, i));
+      if (diff !== 0) return diff;
+    }
+  }
+}
+
+function getSortedKeys(difficulty, mappingFn) {
+  return getKeys(difficulty).sort(getComparator(mappingFn));
+}
+
+function sortedStringify(obj) {
+  return JSON.stringify(obj, getSortedKeys(obj, getKeyIndex).flat());
+}
+
+function deepEquals(a, b) {
+  return sortedStringify(a) === sortedStringify(b);
+}
+
+function formatDifficulty(path) {
+  const difficulty = JSON.parse(fs.readFileSync(path));
+  for (let key of Object.keys(difficulty)) {
+    if (deepEquals(difficulty[key], base[key])) {
+      delete difficulty[key];
+    }
+  }
+  fs.writeFileSync(path, JSON.stringify(difficulty, getSortedKeys(difficulty, getKeyIndexDifficulty).flat(), 2));
+}
+
+for (let path of paths) {
+  formatDifficulty(path);
+}
